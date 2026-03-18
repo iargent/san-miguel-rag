8d0cae3a881f970d800b3563bb6a73b4d8668477
Iain Argent <iainargent@googlemail.com>
2026-03-18 16:55:35 +0100

    Add build step and informational messages

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+set -e
+
+./build.sh
+
+echo "Uploading to S3..."
 aws s3 cp deployment.zip s3://cv-optimiser-iain-argent/san-miguel-rag/deployment.zip \
     --region eu-west-1
 
+echo "Updating Lambda..."
 aws lambda update-function-code \
     --function-name san-miguel-rag \
     --s3-bucket cv-optimiser-iain-argent \
     --s3-key san-miguel-rag/deployment.zip \
     --region eu-west-1
+    
+echo "Deployment complete."
+
